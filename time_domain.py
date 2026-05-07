160ec4c62a4634220c35dbceb4f109c1def91239
yahia <y_kilany@aucegypt.edu>
2026-05-07 18:34:23 +0300

    flatten time-domain features

--- time_domain.py
+++ time_domain.py
@@ -1,33 +1,53 @@
-import scipy.io as spio
 import numpy as np
-def time_domain( EEG: dict, n: float = 0.5 ) -> tuple:
+import scipy.io as sio
+def time_domain(EEG: dict, n: float = 0.5) -> tuple:
     fs = EEG["fs"][0][0]
     n_samples = int(n * fs)
     n_trials = len(EEG["data"])
     n_channels = len(EEG["data"][0])
     n_windows = EEG["data"][0][0].shape[0] // n_samples
     n_features = 7
+
+    stat_desc = np.zeros((n_trials * n_windows, n_channels * n_features))
     
-    arousal = np.zeros(n_trials)
-    valence = np.zeros(n_trials)
-    stat_desc = np.zeros((n_trials, n_channels, n_windows, n_features))
-    for i, trial in enumerate(EEG["data"]):
-        valence[i] = EEG["labels"][i][0]
-        arousal[i] = EEG["labels"][i][1]
-        for j,waves in enumerate(trial):
+    arousal = np.repeat([label[1] for label in EEG["labels"]], n_windows)
+    valence = np.repeat([label[0] for label in EEG["labels"]], n_windows)
+
+    for i in range(n_trials):
+        for j in range(n_channels):
+            waves = EEG["data"][i][j]
             offset = 0
             wind_idx = 0
-            print(f"Processing trial {i}, channel {j}...")
+            
             while offset + n_samples <= waves.shape[0]:
-                window = EEG["data"][i][j][offset:offset + n_samples]
+                window = waves[offset:offset + n_samples]
+                
+                # Feature calculations
                 mean = window.mean()
                 std = window.std()
                 rms = np.sqrt(np.mean(window**2))
-                max = np.max(window)
+                mx = np.max(window)
                 skewness = (np.mean((window - mean)**3)) / (std**3)
                 kurtosis = (np.mean((window - mean)**4)) / (std**4) - 3
                 energy = np.sum(window**2)
+                
+                # row: which window are we in 
+                row = (i * n_windows) + wind_idx 
+
+                # Column: which group of features (channel) are we in?
+                col_start = j * n_features
+                col_end = col_start + n_features
+                
+                stat_desc[row, col_start:col_end] = [mean, std, rms, mx, skewness, kurtosis, energy]
+                
                 offset += n_samples
-                stat_desc[i][j][wind_idx] = [mean, std, rms, max, skewness, kurtosis, energy]
                 wind_idx += 1
-    return stat_desc, valence, arousal  
+                
+    return np.array(stat_desc), np.array(valence), np.array(arousal)
+
+if __name__ == "__main__":
+    data = sio.loadmat('s01.mat')
+    stat_desc, valence, arousal = time_domain(data)
+    print(stat_desc.shape)
+    print(valence.shape)
+    print(arousal.shape)

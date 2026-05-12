a59c1f1969460d9d1d008fda69bd728fe833e403
joshuamakar <joshuamakar@aucegypt.edu>
2026-05-12 19:37:48 +0300

    updated freq_domain method
    used better method to compute power of freq bands

--- freq_domain.py
+++ freq_domain.py
@@ -1,7 +1,5 @@
 import numpy as np
 import scipy.io as sio
-from scipy.signal import welch
-from scipy.integrate import trapezoid
 
 bands = {
     "delta": (0.5, 4),
@@ -14,48 +12,59 @@ bands = {
 def freq_domain(EEG: dict, n: float = 0.5) -> tuple:
     fs = EEG["fs"][0][0]
     n_samples = int(n * fs)
+
     n_trials = len(EEG["data"])
     n_channels = len(EEG["data"][0])
     n_windows = EEG["data"][0][0].shape[0] // n_samples
     n_features = len(bands)
+
     band_pow = np.zeros((n_trials * n_windows, n_channels * n_features))
-    
+
     arousal = np.repeat([label[1] for label in EEG["labels"]], n_windows)
     valence = np.repeat([label[0] for label in EEG["labels"]], n_windows)
-    
-    # nperseg can't exceed the segment length
-    nperseg = min(n_samples, 128)
-    
+
     for i in range(n_trials):
         for j in range(n_channels):
             waves = EEG["data"][i][j]
+
             offset = 0
             wind_idx = 0
-            
+
             while offset + n_samples <= waves.shape[0]:
                 window = waves[offset:offset + n_samples]
-                
-                # PSD via Welch, then integrate over each band
-                freqs, psd = welch(window, fs=fs, nperseg=nperseg)
+                window = window - np.mean(window)
+
+                fft_values = np.fft.rfft(window)
+                freqs = np.fft.rfftfreq(n_samples, d=1/fs)
+                power_spectrum = (np.abs(fft_values) ** 2) / n_samples
+
                 feats = []
-                for low, high in bands.values():
-                    mask = (freqs >= low) & (freqs < high)
-                    feats.append(trapezoid(psd[mask], freqs[mask]))
-                
+
+                for band_idx, (low, high) in enumerate(bands.values()):
+                    if band_idx == len(bands) - 1:
+                        mask = (freqs >= low) & (freqs <= high)
+                    else:
+                        mask = (freqs >= low) & (freqs < high)
+
+                    band_power = np.sum(power_spectrum[mask])
+                    feats.append(band_power)
+
                 row = (i * n_windows) + wind_idx
                 col_start = j * n_features
                 col_end = col_start + n_features
-                
+
                 band_pow[row, col_start:col_end] = feats
-                
+
                 offset += n_samples
                 wind_idx += 1
-                
+
     return np.array(band_pow), np.array(valence), np.array(arousal)
 
+
 if __name__ == "__main__":
-    data = sio.loadmat('s01.mat')
+    data = sio.loadmat('D:\AUC\Spring 2026\DSP\Project\Data\s01.mat')#make sure to change this path to the correct one 
     band_pow, valence, arousal = freq_domain(data)
-    print(band_pow.shape)
-    print(valence.shape)
-    print(arousal.shape)
+
+    print("Frequency features:", band_pow.shape)
+    print("Valence labels:", valence.shape)
+    print("Arousal labels:", arousal.shape)

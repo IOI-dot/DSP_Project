9dece6c15e81715a4dce2a040a3f35ee24d6fef4
yahia <y_kilany@aucegypt.edu>
2026-05-13 10:50:21 +0300

    updated time_domain function to compute median and variance instead of rms, max, and energy. Also added a small value to the skewness and kurtosis calculations to avoid division by zero.

--- time_domain.py
+++ time_domain.py
@@ -3,10 +3,10 @@ import scipy.io as sio
 def time_domain(EEG: dict, n: float = 0.5) -> tuple:
     fs = EEG["fs"][0][0]
     n_samples = int(n * fs)
-    n_trials = len(EEG["data"])
-    n_channels = len(EEG["data"][0])
-    n_windows = EEG["data"][0][0].shape[0] // n_samples
-    n_features = 7
+    n_trials, n_channels, n_total_samples = EEG["data"].shape
+
+    n_windows = n_total_samples // n_samples
+    n_features = 6
 
     stat_desc = np.zeros((n_trials * n_windows, n_channels * n_features))
     
@@ -15,7 +15,7 @@ def time_domain(EEG: dict, n: float = 0.5) -> tuple:
 
     for i in range(n_trials):
         for j in range(n_channels):
-            waves = EEG["data"][i][j]
+            waves = EEG["data"][i,j]
             offset = 0
             wind_idx = 0
             
@@ -24,12 +24,11 @@ def time_domain(EEG: dict, n: float = 0.5) -> tuple:
                 
                 # Feature calculations
                 mean = window.mean()
+                median = np.median(window)
+                variance = window.var()
                 std = window.std()
-                rms = np.sqrt(np.mean(window**2))
-                mx = np.max(window)
-                skewness = (np.mean((window - mean)**3)) / (std**3)
-                kurtosis = (np.mean((window - mean)**4)) / (std**4) - 3
-                energy = np.sum(window**2)
+                skewness = (np.mean((window - mean)**3)) / (std**3 + 1e-8) # Adding small value to avoid division by zero
+                kurtosis = (np.mean((window - mean)**4)) / (std**4 + 1e-8) - 3 
                 
                 # row: which window are we in 
                 row = (i * n_windows) + wind_idx 
@@ -38,7 +37,7 @@ def time_domain(EEG: dict, n: float = 0.5) -> tuple:
                 col_start = j * n_features
                 col_end = col_start + n_features
                 
-                stat_desc[row, col_start:col_end] = [mean, std, rms, mx, skewness, kurtosis, energy]
+                stat_desc[row, col_start:col_end] = [mean, median, variance, std, skewness, kurtosis]
                 
                 offset += n_samples
                 wind_idx += 1
@@ -47,6 +46,7 @@ def time_domain(EEG: dict, n: float = 0.5) -> tuple:
 
 if __name__ == "__main__":
     data = sio.loadmat('s01.mat')
+    print(data["data"].shape)
     stat_desc, valence, arousal = time_domain(data)
     print(stat_desc.shape)
     print(valence.shape)
